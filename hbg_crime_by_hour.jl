5a79eb2dc75837f5aeeb8b65b8568e5a363f1938
Joshua Miller <elefantstn@gmail.com>
2014-01-20 20:28:15 -0800

    Fix crime by hour stats plotting.

--- hbg_crime_by_hour.jl
+++ hbg_crime_by_hour.jl
@@ -1,20 +1,23 @@
 using DataFrames
 using Datetime
-
-data = readtable("reports.csv")
-
-formatter = "yyyy-MM-ddTHH:mm:ss"
-
-function eachHour(m)
-    map(h -> Datetime.hour(Datetime.datetime(formatter, h)), m)
-end
-
-withHours = @transform(data, Hour => eachHour(End))
-
-results = by(withHours, ["Neighborhood", "Hour"], nrow)
-
 using Gadfly
 
-p = plot(results, y="x1", x="Hour", color="Neighborhood", Guide.XLabel("Hour of Day"), Guide.YLabel("Number of Reports"), Geom.bar)
+# All current reports
+# wget http://hbg-crime.org/reports.csv
+data = readtable("reports.csv")
 
-draw(D3("results.js", 9inch, 9inch), p)
+# Parse date strings into the hour of day (0-23)
+formatter = "yyyy-MM-ddTHH:mm:ss"
+function hourofday(d::String)
+    Datetime.hour(Datetime.datetime(formatter, d))
+end
+@vectorize_1arg String hourofday
+@transform(data, Hour => hourofday(End))
+
+# Group by Neighborhood; strip unclassified reports
+results = by(data, ["Neighborhood", "Hour"], nrow)
+complete_cases!(results)
+
+# Plot and draw!
+p = plot(results, y="x1", x="Hour", color="Neighborhood", Guide.XLabel("Hour of Day"), Guide.YLabel("Number of Reports"), Geom.bar(position=:dodge))
+draw(SVG("results.svg", 9inch, 9inch), p)
