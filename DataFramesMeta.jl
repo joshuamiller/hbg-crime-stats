4d3e39b7a38350e38e2648e91eb41c680b494043
Joshua Miller <josh@joshmiller.io>
2014-10-10 19:27:59 -0700

    Update K-Nearest Neighbor classification.
    Now works with Julia 0.3.1 and DataFrames 0.5.10.

---
+++ DataFramesMeta.jl
@@ -0,0 +1,259 @@
+# Via https://github.com/JuliaStats/DataFramesMeta.jl
+
+module DataFramesMeta
+
+importall Base
+importall DataFrames
+using DataFrames
+
+# Basics:
+export @with, @ix, @where, @orderby, @transform, @by, @based_on, @select
+export where, orderby, transform, select 
+
+
+##############################################################################
+##
+## @with
+##
+##############################################################################
+
+replace_syms(x, membernames) = x
+function replace_syms(e::Expr, membernames)
+    if e.head == :call && length(e.args) == 2 && e.args[1] == :^
+        return e.args[2]
+    elseif e.head == :.     # special case for :a.b
+        return Expr(e.head, replace_syms(e.args[1], membernames),
+                            typeof(e.args[2]) == Expr && e.args[2].head == :quote ? e.args[2] : replace_syms(e.args[2], membernames))
+    elseif e.head != :quote
+        return Expr(e.head, (isempty(e.args) ? e.args : map(x -> replace_syms(x, membernames), e.args))...)
+    else
+        if haskey(membernames, e.args[1])
+            return membernames[e.args[1]]
+        else
+            a = gensym()
+            membernames[e.args[1]] = a
+            return a
+        end
+    end
+end
+
+function with_helper(d, body)
+    membernames = Dict{Symbol, Symbol}()
+    body = replace_syms(body, membernames)
+    funargs = map(x -> :( getindex($d, $(Meta.quot(x))) ), collect(keys(membernames)))
+    funname = gensym()
+    return(:( function $funname($(collect(values(membernames))...)) $body end; $funname($(funargs...)) ))
+end
+
+macro with(d, body)
+    esc(with_helper(d, body))
+end
+
+
+##############################################################################
+##
+## @withfirst - helper
+##            - @withfirst( fun(df, :a) ) becomes @with(df, fund(df, :a))
+##
+##############################################################################
+
+function withfirst_helper(e)
+    quote
+        let d = $(e.args[2]); 
+            @with(d, $(e.args[1])(d, $(e.args[3:end]...)))
+        end
+    end
+end
+
+macro withfirst(e)
+    esc(withfirst_helper(e))
+end
+
+
+##############################################################################
+##
+## @ix - row and row/col selector
+##
+##############################################################################
+
+ix_helper(d, arg) = :( let d = $d; $d[@with($d, $arg),:]; end )
+ix_helper(d, arg, moreargs...) = :( let d = $d; getindex(d, @with(d, $arg), $(moreargs...)); end )
+
+macro ix(d, args...)
+    esc(ix_helper(d, args...))
+end
+
+
+##############################################################################
+##
+## @where - select row subsets
+##
+##############################################################################
+
+where(d::AbstractDataFrame, arg) = d[arg, :]
+where(d::AbstractDataFrame, f::Function) = d[f(d), :]
+where(g::GroupedDataFrame, f::Function) = g[Bool[f(x) for x in g]]
+
+## macro where(d, arg)
+##     esc(:( @withfirst where($d, $arg) ))
+## end
+
+
+where_helper(d, arg) = :( where($d, _DF -> @with(_DF, $arg)) )
+
+macro where(d, arg)
+    esc(where_helper(d, arg))
+end
+
+
+##############################################################################
+##
+## select - select columns
+##
+##############################################################################
+
+select(d::AbstractDataFrame, arg) = d[:, arg]
+
+
+##############################################################################
+##
+## @orderby
+##
+##############################################################################
+
+function orderby(d::AbstractDataFrame, args...)
+    D = typeof(d)(args...)
+    d[sortperm(D), :]
+end
+orderby(d::AbstractDataFrame, f::Function) = d[sortperm(f(d)), :]
+orderby(g::GroupedDataFrame, f::Function) = g[sortperm([f(x) for x in g])]
+orderbyconstructor(d::AbstractDataFrame) = typeof(d)
+orderbyconstructor(d::DataFrame) = DataFrame
+orderbyconstructor(d) = x -> x
+
+macro orderby(d, args...)
+    esc(:(let _D = $d;  orderby(_D, _DF -> @with(_DF, DataFramesMeta.orderbyconstructor(_D)($(args...)))); end))
+end
+
+
+##############################################################################
+##
+## transform & @transform
+##
+##############################################################################
+
+function transform(d::Union(AbstractDataFrame, Associative); kwargs...)
+    result = copy(d)
+    for (k, v) in kwargs
+        result[k] = isa(v, Function) ? v(d) : v
+    end
+    return result
+end
+
+function transform(g::GroupedDataFrame; kwargs...)
+    result = DataFrame(g)
+    idx2 = cumsum(Int[size(g[i],1) for i in 1:length(g)])
+    idx1 = [1, 1 + idx2[1:end-1]]
+    for (k, v) in kwargs
+        first = v(g[1])
+        result[k] = Array(eltype(first), size(result, 1))
+        result[idx1[1]:idx2[1], k] = first
+        for i in 2:length(g)
+            result[idx1[i]:idx2[i], k] = v(g[i])
+        end
+    end
+    return result
+end
+
+
+function transform_helper(x, args...)
+    # convert each kw arg value to: _DF -> @with(_DF, arg)
+    newargs = [args...]
+    for i in 1:length(args)
+        newargs[i].args[2] = :( _DF -> @with(_DF, $(newargs[i].args[2]) ) )
+    end
+    :( transform($x, $(newargs...)) )
+end
+
+macro transform(x, args...)
+    esc(transform_helper(x, args...))
+end
+
+
+
+##############################################################################
+##
+## @based_on - summarize a grouping operation
+##
+##############################################################################
+
+macro based_on(x, args...)
+    esc(:( DataFrames.based_on($x, _DF -> @with(_DF, DataFrame($(args...)))) ))
+end
+
+
+##############################################################################
+##
+## @by - grouping
+##
+##############################################################################
+
+macro by(x, what, args...)
+    esc(:( by($x, $what, _DF -> @with(_DF, DataFrame($(args...)))) ))
+end
+
+
+##############################################################################
+##
+## @select - select and transform columns
+##
+##############################################################################
+
+expandargs(x) = x
+
+function expandargs(e::Expr) 
+    if e.head == :quote && length(e.args) == 1
+        return Expr(:kw, e.args[1], Expr(:quote, e.args[1]))
+    else
+        return e
+    end
+end
+
+function expandargs(e::Tuple)
+    res = [e...]
+    for i in 1:length(res)
+        res[i] = expandargs(e[i])
+    end
+    return res
+end
+
+function select(d::Union(AbstractDataFrame, Associative); kwargs...)
+    result = typeof(d)()
+    for (k, v) in kwargs
+        result[k] = v
+    end
+    return result
+end
+
+macro select(x, args...)
+    esc(:(let x = $x; @with(x, select(x, $(expandargs(args)...))); end))
+end
+
+
+##############################################################################
+##
+## Extras for GroupedDataFrames
+##
+##############################################################################
+
+combnranges(starts, ends) = [[starts[i]:ends[i] for i in 1:length(starts)]...]
+
+DataFrame(g::GroupedDataFrame) = g.parent[g.idx[combnranges(g.starts, g.ends)], :]
+
+Base.getindex(gd::GroupedDataFrame, I::AbstractArray{Int}) = GroupedDataFrame(gd.parent,
+                                                                              gd.cols,
+                                                                              gd.idx,
+                                                                              gd.starts[I],
+                                                                              gd.ends[I])
+
+end # module
--- knn_classification.jl
+++ knn_classification.jl
@@ -1,18 +1,19 @@
 using DataFrames
-require("Datetime")
 using Datetime
+require("DataFramesMeta")
+using DataFramesMeta
 
 reports = readtable("reports.csv")
 
 # Remove start time and neighborhood, then only use
 # complete cases from there
-delete!(reports, ["Start", "Neighborhood"])
+delete!(reports, [:Start, :Neighborhood])
 complete_cases!(reports)
 
-max_lat = maximum(removeNA(reports["Lat"]))
-min_lat = minimum(removeNA(reports["Lat"]))
-max_lon = maximum(removeNA(reports["Lon"]))
-min_lon = minimum(removeNA(reports["Lon"]))
+max_lat = maximum(dropna(reports[:Lat]))
+min_lat = minimum(dropna(reports[:Lat]))
+max_lon = maximum(dropna(reports[:Lon]))
+min_lon = minimum(dropna(reports[:Lon]))
 
 # Using very different units between lat/lon and time;
 # standardize on a 0-1 range
@@ -24,16 +25,15 @@ function standardize(m::DataArray{Float64,1}, max, min)
     (m .- min) ./ (max - min)
 end
 
-@transform(reports, StandardLat => standardize(Lat, max_lat, min_lat))
-@transform(reports, StandardLon => standardize(Lon, max_lon, min_lon))
-
+reports = @transform(reports, StandardLat = standardize(:Lat, max_lat, min_lat))
+reports = @transform(reports, StandardLon = standardize(:Lon, max_lon, min_lon))
 
 # Make the End column a date
 function parsedate(string::String)
     Datetime.datetime("yyyy-MM-ddTHH:mm:ss", string)
 end
 @vectorize_1arg String parsedate
-@transform(reports, End => parsedate(End))
+reports = @transform(reports, End = parsedate(:End))
 
 # Standardized minutes on a 0.0-1.0 scale, time from noon
 function standardize_minutes(d::DateTime)
@@ -42,15 +42,15 @@ function standardize_minutes(d::DateTime)
     dist_to_noon / 720
 end
 @vectorize_1arg Any standardize_minutes
-@transform(reports, StandardMin => standardize_minutes(End))
+reports = @transform(reports, StandardMin = standardize_minutes(:End))
 
 # Standardize day of week on a 0.0-1.0 scale
 function standardize_day(d::DateTime)
     (Datetime.dayofweek(d) - 1) / 6
 end
 @vectorize_1arg Any standardize_day
-@transform(reports, StandardDay => standardize_day(End))
- 
+reports = @transform(reports, StandardDay = standardize_day(:End))
+
 # Want to generalize this in terms of two vectors
 function eucl_dist(lat1, lon1, min1, day1, lat2, lon2, min2, day2)
     (lat2 - lat1)^2 +
@@ -65,9 +65,9 @@ function nearest(lat, lon, time, k)
     std_lon = standardize(lon, max_lon, min_lon)
     std_min = standardize_minutes(time)
     std_day = standardize_day(time)
-    reports["Distances"] = [eucl_dist(std_lat, std_lon, std_min, std_day, lat, lon, min, day) for (lat,lon,min,day)=zip(reports["StandardLat"], reports["StandardLon"], reports["StandardMin"], reports["StandardDay"])]
-    sorted = sortby(reports, "Distances")
-    last(sortby(by(sorted[1:k, ["Description"]], "Description", nrow), "x1")[:1])
+    reports[:Distances] = [eucl_dist(std_lat, std_lon, std_min, std_day, lat, lon, min, day) for (lat,lon,min,day)=zip(reports[:StandardLat], reports[:StandardLon], reports[:StandardMin], reports[:StandardDay])]
+    sorted = @orderby(reports, :Distances)
+    last(@orderby(by(DataFrame(sorted[1:k, :Description]), :x1, size), :x1_1)[:1])
 end
-     
+
 println(nearest(40.2821445, -76.8804254, Datetime.now(), 7))
